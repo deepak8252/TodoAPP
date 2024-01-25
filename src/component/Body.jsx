54f579284aa74457cc26ccb1a251efca067c59d3
Deepak kumar <deepak.kumar.82521@gmail.com>
2024-01-25 14:02:00 +0530

    upload the to do app full responsive

--- src/component/Body.jsx
+++ src/component/Body.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { addtodataremove } from '../Appstore/addtoSlice';
- 
+import DeleteIcon from '@mui/icons-material/Delete';
+import { Button } from '@mui/material';
+
 const Body = () => {
     const addtoitems=useSelector(state=>state.items);
     const dispatch=useDispatch()
@@ -12,10 +14,10 @@ const Body = () => {
     <div className='bg-green-600 text-white p-3  w-full mx-auto'>
 {
     addtoitems && addtoitems.map((item)=>(
-      <div key={item.id} className='flex justify-between'>
+      <div key={item.id} className='flex justify-between border-b-2'>
     <div className='p-2 capitalize text-2xl w-full'>{item.text}</div>
-    <div>
-        <button className='bg-red-600 p-2 capitalize text-2xl my-3' onClick={()=>deletedata(item.id)}>delete</button>
+    <div className='m-3'>
+        <Button variant='contained' color='error'  onClick={()=>deletedata(item.id)}><DeleteIcon/></Button>
     </div>
       </div>
       
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import {  useDispatch } from 'react-redux';
 import { addtodo } from '../Appstore/addtoSlice';
+import Button from '@mui/material/Button';
+import Menu from './Menu';
+import { TextField } from '@mui/material';
 const Header = () => {
     const[text,settext]=useState();
     const dispatch=useDispatch();
@@ -9,16 +12,19 @@ const Header = () => {
     dispatch(addtodo(text));
     settext("");
  }
-  return (
+  return (  
+   <>
+    <Menu/>
     <div className=' lg:9/12 sm:6/12 sm:w-full  mx-auto shadow-lg p-2 bg-gray-300'>
-        <h2 className=' capitalize bg-blue-800 text-white m-4 p-4 text-2xl text-center' >to do app</h2>
-        <form action="" onSubmit={handleadd}>
-           <input type="text" className=' border-gray-600 lg:w-9/12 md:w-6/12 sm:w-full p-4  rounded border-2' placeholder='enter your todo list' value={text} onChange={(e)=>{
+        <form action="" onSubmit={handleadd} className='mx-auto'>
+        <TextField id="standard-basic" label="Add to do list " variant="standard" value={text}onChange={(e)=>{
             settext(e.target.value)
            }} />
-           <button className=' bg-teal-800 text-white px-2 py-3 m-3 capitalize text-2xl mx-3 text-center' >submit </button>
+            
+           <button  > <Button variant='contained' color='success'>submit</Button></button>
         </form>
     </div>
+   </>
   )
 }
 
