e4a4da8548fcefb5f9c437ce8346c2f02c9c81cd
Deepak kumar <deepak.kumar.82521@gmail.com>
2024-01-25 12:58:00 +0530

    2nd time

--- src/component/Body.jsx
+++ src/component/Body.jsx
@@ -8,13 +8,12 @@ const Body = () => {
     const deletedata=(itemid)=>{
    dispatch(addtodataremove(itemid))
     }
-console.log(addtoitems+"hello");
   return (
-    <div className='bg-green-600 text-white p-3  w-6/12 mx-auto'>
+    <div className='bg-green-600 text-white p-3  w-full mx-auto'>
 {
     addtoitems && addtoitems.map((item)=>(
       <div key={item.id} className='flex justify-between'>
-    <div className='p-2 capitalize text-2xl'>{item.text}</div>
+    <div className='p-2 capitalize text-2xl w-full'>{item.text}</div>
     <div>
         <button className='bg-red-600 p-2 capitalize text-2xl my-3' onClick={()=>deletedata(item.id)}>delete</button>
     </div>
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -10,10 +10,10 @@ const Header = () => {
     settext("");
  }
   return (
-    <div className=' w-6/12 mx-auto shadow-lg p-2 bg-gray-300'>
+    <div className=' lg:9/12 sm:6/12 sm:w-full  mx-auto shadow-lg p-2 bg-gray-300'>
         <h2 className=' capitalize bg-blue-800 text-white m-4 p-4 text-2xl text-center' >to do app</h2>
         <form action="" onSubmit={handleadd}>
-           <input type="text" className=' border-gray-600 w-9/12 p-4  rounded border-2' placeholder='enter your todo list' value={text} onChange={(e)=>{
+           <input type="text" className=' border-gray-600 lg:w-9/12 md:w-6/12 sm:w-full p-4  rounded border-2' placeholder='enter your todo list' value={text} onChange={(e)=>{
             settext(e.target.value)
            }} />
            <button className=' bg-teal-800 text-white px-2 py-3 m-3 capitalize text-2xl mx-3 text-center' >submit </button>
